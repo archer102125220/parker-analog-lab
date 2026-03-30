507f84a930d47059fc0f66cbab96f7b87d118a15
Parker Chen <archer102125220.2015@gmail.com>
2026-03-30 13:52:08 +0800

    feat: add HTTPS support via vite-plugin-mkcert and dev:https script

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,30 @@
 import { defineConfig } from 'vite';
 import analog from '@analogjs/platform';
 import tailwindcss from '@tailwindcss/vite';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  build: {
-    target: ['es2020'],
-  },
-  resolve: {
-    mainFields: ['module'],
-  },
-  plugins: [
-    analog(),
-    tailwindcss()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/test-setup.ts'],
-    include: ['**/*.spec.ts'],
-    reporters: ['default'],
-  },
-}));
+export default defineConfig(({ mode }) => {
+  const isHttps = process.env['HTTPS'] === 'true';
+
+  return {
+    build: {
+      target: ['es2020'],
+    },
+    resolve: {
+      mainFields: ['module'],
+    },
+    plugins: [
+      analog(),
+      tailwindcss(),
+      isHttps ? mkcert() : undefined
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test-setup.ts'],
+      include: ['**/*.spec.ts'],
+      reporters: ['default'],
+    },
+  };
+});
